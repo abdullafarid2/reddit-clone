277175b4364dd514eeae3869aefd39776a8e5487
abdullafarid2 <79112549+abdullafarid2@users.noreply.github.com>
2022-12-26 17:37:20 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["links.papareact.com", "avatars.dicebear.com"],
+    domains: ["links.papareact.com", "avatars.dicebear.com", "logos-world.net"],
   },
 };
